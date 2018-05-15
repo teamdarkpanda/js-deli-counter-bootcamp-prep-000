77bd7539c26978b70e7d026c13fe5c1fa35795f5
Ankur Puri <apuri@iit.edu>
2018-05-15 23:30:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function takeANumber(katzDeliLine, name) {
-  if(katzDeliLine.length === 0) {
-    katzDeliLine.push(name)
-    return string `"Welcome, ${name}. You are number 1 in line."`  
-  }
   katzDeliLine.push(name)
-  return string = `"Welcome, ${name}. You are number " + katzDeliLine.length + 1 + " in line."`
+  var position = katzDeliLine.length
+  return string = `"Welcome, ${name}. You are number ${position} in line."`
 }
