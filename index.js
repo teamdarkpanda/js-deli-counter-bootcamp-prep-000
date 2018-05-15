b6f16fd9e3112a9b7d0da9b3500d7465114aa513
Ankur Puri <apuri@iit.edu>
2018-05-15 23:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
-    return ``
+    return `There is nobody waiting to be served!`
   }
   var name = katzDeliLine.shift()
   console.log(name)
