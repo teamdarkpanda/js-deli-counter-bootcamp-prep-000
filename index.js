63f0b1d8538064116c587447893122796405e097
Ankur Puri <apuri@iit.edu>
2018-05-15 23:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
-    return `There is nobody waiting to be served!`
+    return `The line is currently empty.`
   }
+  
 }
