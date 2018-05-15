56d61325ca63dc552347c702544ff1294e768e3a
Ankur Puri <apuri@iit.edu>
2018-05-15 23:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  var position = katzDeliLine.length + 1
+  var position = katzDeliLine.length
   return `"Welcome, ${name}. You are number ${position} in line."`
 }
