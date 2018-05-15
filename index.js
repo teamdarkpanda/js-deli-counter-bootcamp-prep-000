3b5352b642375873290f11f401d27c3f739f1d4d
Ankur Puri <apuri@iit.edu>
2018-05-15 23:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return `The line is currently empty.`
   }
-  var names = ""
-  
+  var names = "The Line is currently: "
+  for(var i = 0; i < katzDeliLine.length; i++) {
+    
+  }
 }
